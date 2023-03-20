5d0a41a329203d0ca7af17a31a963fc3be31d7ff
Valimp <valentinchauvet.contact@gmail.com>
2023-03-20 17:00:13 +0100

    First Commit

---
+++ src/components/Navigate.vue
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<template>
+    <header class="header">
+        <h1>
+            CMPhoto
+        </h1>
+        <nav class="menu">
+            <ul class="links">
+                <li class="link">
+                    <a href="#">Qui suis-je ?</a>
+                </li>
+                <li class="link">
+                    <a href="#">Portfolio</a>
+                </li>
+                <li class="link">
+                    <a href="#">Mes offres</a>
+                </li>
+                <li class="link">
+                    <a href="#">Me contacter</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        padding: 10px 20px;
+        background-color: #0000006b;
+        position: sticky;
+        top: 0;
+        left: 0;
+    }
+    .links {
+        display: flex;
+    }
+    .link {
+        list-style-type: none;
+        margin: 0 20px;
+    }
+</style>
---
+++ src/components/Section.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<template>
+    <section class="section">
+
+    </section>
+</template>
+
+<style scoped>
+    .section {
+        height: 40rem;
+        background-color: #333;
+        clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
+        margin: 5rem 0;
+    }
+</style>
