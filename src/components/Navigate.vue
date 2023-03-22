391b0592cbd711a50bbbf28f7f9fa3311c68e1ca
Valentin Chauvet <valentinchauvet.contact@gmail.com>
2023-03-22 12:59:22 +0100

    Naviguate underline

--- src/components/Navigate.vue
+++ src/components/Navigate.vue
@@ -3,8 +3,8 @@
 
 <template>
     <header class="header">
-        <h1>
-            CMPhoto
+        <h1 class="title">
+            PhotoCM
         </h1>
         <nav class="menu">
             <ul class="links">
@@ -36,6 +36,11 @@
         position: sticky;
         top: 0;
         left: 0;
+        z-index: 5;
+        color: white;
+    }
+    .title {
+        font-family: 'Delicious Handrawn', cursive;
     }
     .links {
         display: flex;
@@ -43,5 +48,26 @@
     .link {
         list-style-type: none;
         margin: 0 20px;
+        transform: all .2s;
+    }
+    .link a {
+        color: white;
+        position: relative;
+    }
+    .link  a::after {
+        content: '';
+        height: 1.5px;
+        background-color: orange;
+        transform-origin: 0 0;
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+        right: 0;
+        transform: scaleX(0);
+        transition: transform .4s ease-in-out;
+        border-radius: 1px;
+    }
+    .link a:hover::after {
+        transform: scaleX(1);
     }
 </style>
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -9,9 +9,10 @@
 
 <style scoped>
     .section {
-        height: 40rem;
+        height: 30rem;
         background-color: #333;
         clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
-        margin: 5rem 0;
+        margin: 15rem 0;
+        width: 100%;
     }
 </style>
